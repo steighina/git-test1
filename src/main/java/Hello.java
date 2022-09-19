2fc472c3bc9dc5344f0bc604053dbf9dd1517dcd
steighina <steighina@163.com>
2022-09-19 15:57:07 +0800

    进行一次本地git提交测试

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Description
+ * @Author wangkai
+ * @Date 2022/9/19 15:54
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Git!");
+    }
+}
